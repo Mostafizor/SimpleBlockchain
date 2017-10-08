9a3739cc89f1789adea690bb9b540d0afb26cd7c
Mostafizor <mostafizor1997@gmail.com>
2017-10-08 11:39:58 +0100

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -6,14 +6,23 @@ class Block(object):
 		self.timestamp = timestamp
 		self.data = data
 		self.previousHash = previousHash
+		self.nonce = 0
 		self.hash = self.calculateHash()
 
 	def calculateHash(self):
 		return hs.sha256(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previousHash)).hexdigest()
-
+							 
+	def mineBlock(self, difficulty):
+		print('Mining Block...')
+		while self.hash[0:difficulty] != ''.join(['0'] * difficulty):
+			self.nonce += 1
+			self.hash = self.calculateHash()
+		print('Block Mined: %s' %(self.hash))
+							
 class Blockchain(object):
 	def __init__(self):
 		self.chain = [self.createGenesisBlock()]
+		self.difficulty = 5
 
 	def createGenesisBlock(self):
 		return Block('0', '25/09/2017', 'Genesis Block', '0')
@@ -23,7 +32,7 @@ class Blockchain(object):
 
 	def addBlock(self, newBlock):
 		newBlock.previousHash = self.getLatestBlock().hash
-		newBlock.hash = newBlock.calculateHash()
+		newBlock.mineBlock(self.difficulty)
 		self.chain.append(newBlock)
 
 	def isChainValid(self):
@@ -47,18 +56,6 @@ coin.addBlock(Block('3', '25/09/2017', 'Satoshi sent 2000 coins to Mo'))
 print('Is Chain Valid ?: %s' %(coin.isChainValid()))
 
 # Print Block Chain Data
-for b in range(len(coin.chain)):
-	print('Block: %s' %(b), vars(coin.chain[b]))
-	print('')
-
-# Tamper with the Blockchain
-coin.chain[2].data = 'Satoshi sent 50000 coins to Mo'
-coin.chain[2].hash = coin.chain[2].calculateHash()
-
-# Check validity of Blockchain after tampering
-print('Is Chain Valid ?: %s' %(coin.isChainValid()))
-
-# Print tampered Blockchain data
 for b in range(len(coin.chain)):
 	print('Block: %s' %(b), vars(coin.chain[b]))
 	print('')
